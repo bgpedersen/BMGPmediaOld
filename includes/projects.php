19af23d2b3342177e5bc063ae01dbd2b46acd7bd
Bjarne Mølgaard Gerhardt-Pedersen <bjarne_g_pedersen@hotmail.com>
2015-04-14 20:25:56 +0200

    BMGPmedia v1 færdig

--- includes/projects.php
+++ includes/projects.php
@@ -23,44 +23,39 @@
 						<br>
 						<p>To log in and use Logic Lead System:</p>
 						<ul>
-							<li>Employee: testuser@hotmail.com // Password: Testuser1</li>
-							<li>Admin: testadmin@hotmail.com // Password: Testadmin1</li>
+							<li>testuser@hotmail.com // Testuser1</li>
+							<li>testadmin@hotmail.com // Testadmin1</li>
 						</ul>
 						<p>Code can be found on <a href="https://github.com/Moelgaard85/LogicLeadSystem/" target="_blank">Github</a>
 						</p>
-						<a href="#" target="_blank"><button type="button" class="btn btn-large btn-info">See the work</button></a>
+						<a href="http://bmgpmedia.dk/logicleadsystem/" target="_blank"><button type="button" class="btn btn-large btn-info">See the work</button></a>
 					</div>
 				</div><!-- col left description end -->
 				<div class="col-md-8 col-xs-12">
 					<div class="project_content_right">
 						<div class="project-image">
 
-						<!-- Carousel 1 -->
+							<!-- Carousel 1 -->
 							<div id="Carousel1" class="carousel slide" data-ride="carousel">
 								<!-- Indicators -->
 								<ol class="carousel-indicators">
 									<li data-target="#Carousel1" data-slide-to="0" class="active"></li>
 									<li data-target="#Carousel1" data-slide-to="1"></li>
 									<li data-target="#Carousel1" data-slide-to="2"></li>
-									<li data-target="#Carousel1" data-slide-to="3"></li>
 								</ol>
 
 								<!-- Wrapper for slides -->
 								<div class="carousel-inner" role="listbox">
 									<div class="item active">
-										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
+										<img src="images/logicleadsystem_1.jpg" alt="logicleadsystem_1">
 									</div>
 
 									<div class="item">
-										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
+										<img src="images/logicleadsystem_2.jpg" alt="logicleadsystem_2">
 									</div>
 
 									<div class="item">
-										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
-									</div>
-
-									<div class="item">
-										<img src="images/logicleadsystem.jpg" alt="logicleadsystem">
+										<img src="images/logicleadsystem_3.jpg" alt="logicleadsystem_3">
 									</div>
 								</div>
 
@@ -94,53 +89,50 @@
 						<h4>Prattle</h4>
 						<h5>A digital handbook system</h5>
 						<img src="images/grey_line.png" class="line" alt="grey_line">
-						<p>This project is an administrator-controlled digital handbook system for guides and plugins that targets employees of a minor web company. I have developed the system from scratch in PHP and MySQL and used the Bootstrap framwork. The system has been securely developed to protected against attacks.</p>
-						<br>
-						<p>To log in and use Logic Lead System:</p>
-						<ul>
-							<li>Employee: testuser@hotmail.com // Password: Testuser1</li>
-							<li>Admin: testadmin@hotmail.com // Password: Testadmin1</li>
-						</ul>
-						<p>Code can be found on <a href="https://github.com/Moelgaard85/LogicLeadSystem/" target="_blank">Github</a>
-						</p>
-						<a href="#" target="_blank"><button type="button" class="btn btn-large btn-info">See the work</button></a>
-					</div>
-				</div><!-- col left description end -->
-				<div class="col-md-8 col-xs-12">
-					<div class="project_content_right">
-						<div class="project-image">
+						<p>This project is called Prattle and is a website based topic/comment system, that allows users login/register, read topics and write comments on a particular topic.
+							If the user has admin privileges, he can furthermore create/edit/delete topics, and also delete comments. <br><br>
+							The project is developed in the ‘IT Security’module	and has its main focus on making the website as secure as possible. <br><br></p>
+							<p>To log in and use Prattle:</p>
+							<ul>
+								<li>testuserprattle@prattle.dk // testuserprattle</li>
+								<li>testadminprattle@prattle.dk // testadminprattle</li>
+							</ul>
+							<a href="http://bmgpmedia.dk/prattle" target="_blank" alt="Prattle"><button type="button" class="btn btn-large btn-info">See the work</button></a>
+						</div>
+					</div><!-- col left description end -->
+					<div class="col-md-8 col-xs-12">
+						<div class="project_content_right">
+							<div class="project-image">
 
 
-						<!-- Carousel 2 -->
-							<div id="carousel2" class="carousel slide" data-ride="carousel">
-								<!-- Indicators -->
-								<ol class="carousel-indicators">
-									<li data-target="#carousel2" data-slide-to="0" class="active"></li>
-									<li data-target="#carousel2" data-slide-to="1"></li>
-									<li data-target="#carousel2" data-slide-to="2"></li>
-									<li data-target="#carousel2" data-slide-to="3"></li>
-								</ol>
+								<!-- Carousel 2 -->
+								<div id="carousel2" class="carousel slide" data-ride="carousel">
+									<!-- Indicators -->
+									<ol class="carousel-indicators">
+										<li data-target="#carousel2" data-slide-to="0" class="active"></li>
+										<li data-target="#carousel2" data-slide-to="1"></li>
+										<li data-target="#carousel2" data-slide-to="2"></li>
+										
+									</ol>
 
-								<!-- Wrapper for slides -->
-								<div class="carousel-inner" role="listbox">
-									<div class="item active">
-										<img src="images/prattle.jpg" alt="prattle">
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner" role="listbox">
+										<div class="item active">
+											<img src="images/prattle_1.jpg" alt="prattle_1">
+										</div>
+
+										<div class="item">
+											<img src="images/prattle_2.jpg" alt="prattle_2">
+										</div>
+
+										<div class="item">
+											<img src="images/prattle_3.jpg" alt="prattle_3">
+										</div>
+
+
 									</div>
 
-									<div class="item">
-										<img src="images/prattle.jpg" alt="prattle">
-									</div>
-
-									<div class="item">
-										<img src="images/prattle.jpg" alt="prattle">
-									</div>
-
-									<div class="item">
-										<img src="images/prattle.jpg" alt="prattle">
-									</div>
-								</div>
-
-								<!-- Left and right controls -->
+									<!-- Left and right controls -->
 								<!-- <a class="left carousel-control" href="#carousel2" role="button" data-slide="prev">
 									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 									<span class="sr-only">Previous</span>
@@ -169,54 +161,50 @@
 				<div class="col-md-4 col-xs-12">
 					<div class="project_content_left">
 						<h4>BookIt</h4>
-						<h5>A digital handbook system</h5>
+						<h5>A room booking system</h5>
 						<img src="images/grey_line.png" class="line" alt="grey_line">
-						<p>This project is an administrator-controlled digital handbook system for guides and plugins that targets employees of a minor web company. I have developed the system from scratch in PHP and MySQL and used the Bootstrap framwork. The system has been securely developed to protected against attacks.</p>
-						<br>
-						<p>To log in and use Logic Lead System:</p>
-						<ul>
-							<li>Employee: testuser@hotmail.com // Password: Testuser1</li>
-							<li>Admin: testadmin@hotmail.com // Password: Testadmin1</li>
-						</ul>
-						<p>Code can be found on <a href="https://github.com/Moelgaard85/LogicLeadSystem/" target="_blank">Github</a>
-						</p>
-						<a href="#" target="_blank"><button type="button" class="btn btn-large btn-info">See the work</button></a>
-					</div>
-				</div><!-- col left description end -->
-				<div class="col-md-8 col-xs-12">
-					<div class="project_content_right">
-						<div class="project-image">
+						<p>This project is called Bookit and is a website based system that allows users to manage the bookings of rooms with ease. The project is developed in the ‘Database and XML’module and targets the teachers of Copenhagen School of Design and Technology. <br><br>
+							The website is furthermore optimized for multiple browser, its responsive and can be used on a tablet.</p>
+							<br>
+							<p>To log in and use BookIt:</p>
+							<ul>
+								<li>admin@bookit.dk // a</li>
+							</ul>
+							
+							<a href="#" target="_blank"><button type="button" class="btn btn-large btn-info">See the work</button></a>
+						</div>
+					</div><!-- col left description end -->
+					<div class="col-md-8 col-xs-12">
+						<div class="project_content_right">
+							<div class="project-image">
 
-							<!-- Carousel 3-->
-							<div id="carousel3" class="carousel slide" data-ride="carousel">
-								<!-- Indicators -->
-								<ol class="carousel-indicators">
-									<li data-target="#carousel3" data-slide-to="0" class="active"></li>
-									<li data-target="#carousel3" data-slide-to="1"></li>
-									<li data-target="#carousel3" data-slide-to="2"></li>
-									<li data-target="#carousel3" data-slide-to="3"></li>
-								</ol>
+								<!-- Carousel 3-->
+								<div id="carousel3" class="carousel slide" data-ride="carousel">
+									<!-- Indicators -->
+									<ol class="carousel-indicators">
+										<li data-target="#carousel3" data-slide-to="0" class="active"></li>
+										<li data-target="#carousel3" data-slide-to="1"></li>
+										<li data-target="#carousel3" data-slide-to="2"></li>
 
-								<!-- Wrapper for slides -->
-								<div class="carousel-inner" role="listbox">
-									<div class="item active">
-										<img src="images/bookit.png" alt="bookit">
+									</ol>
+
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner" role="listbox">
+										<div class="item active">
+											<img src="images/bookit_1.jpg" alt="bookit_1">
+										</div>
+
+										<div class="item">
+											<img src="images/bookit_2.jpg" alt="bookit_2">
+										</div>
+
+										<div class="item">
+											<img src="images/bookit_3.jpg" alt="bookit_3">
+										</div>
+
 									</div>
 
-									<div class="item">
-										<img src="images/bookit.png" alt="bookit">
-									</div>
-
-									<div class="item">
-										<img src="images/bookit.png" alt="bookit">
-									</div>
-
-									<div class="item">
-										<img src="images/bookit.png" alt="bookit">
-									</div>
-								</div>
-
-								<!-- Left and right controls -->
+									<!-- Left and right controls -->
 								<!-- <a class="left carousel-control" href="#carousel3" role="button" data-slide="prev">
 									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 									<span class="sr-only">Previous</span>
@@ -235,5 +223,159 @@
 		</div><!-- container end -->
 	</article><!-- article end -->
 	<!-- Project 3 end -->
+
+	<!-- Project 4 -->
+	<article>
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-4 col-xs-12">
+					<div class="project_content_left">
+						<h4>The Village</h4>
+						<h5>A small city game</h5>
+						<img src="images/grey_line.png" class="line" alt="grey_line">
+						<p>One of my first projects - A browser based game, made from scratch with HTML, CSS, Javascript, JSON objects and JQuery effects. <br><br> 
+							Remember to create a new user when trying for the first time. <br><br>The user details is stored locally in the browsers cache, allowing the details to be saved and reused. The game is created using NetBeans IDE.</p>
+							<br>							
+							
+							<a href="http://bmgpmedia.dk/village" alt="TheVillage" target="_blank"><button type="button" class="btn btn-large btn-info">See the work</button></a>
+						</div>
+					</div><!-- col left description end -->
+					<div class="col-md-8 col-xs-12">
+						<div class="project_content_right">
+							<div class="project-image">
+
+								<!-- Carousel 4-->
+								<div id="carousel4" class="carousel slide" data-ride="carousel">
+									<!-- Indicators -->
+									<ol class="carousel-indicators">
+										<li data-target="#carousel4" data-slide-to="0" class="active"></li>
+										<li data-target="#carousel4" data-slide-to="1"></li>
+										<li data-target="#carousel4" data-slide-to="2"></li>
+
+									</ol>
+
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner" role="listbox">
+										<div class="item active">
+											<img src="images/village_1.jpg" alt="village_1">
+										</div>
+
+										<div class="item">
+											<img src="images/village_2.jpg" alt="village_2">
+										</div>
+
+										<div class="item">
+											<img src="images/village_3.jpg" alt="village_3">
+										</div>
+
+									</div>
+
+									<!-- Left and right controls -->
+								<!-- <a class="left carousel-control" href="#carousel3" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#carousel3" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a> -->
+							</div><!-- Carousel 4 End -->
+
+						</div>
+					</div>
+				</div><!-- col right image end -->
+				
+			</div><!-- row end -->
+		</div><!-- container end -->
+	</article><!-- article end -->
+	<!-- Project 4 end -->
+
+	<!-- Project 5 -->
+	<article>
+		<div class="container">
+			<div class="row">
+
+				<div class="col-md-4 col-xs-12">
+					<div class="project_content_left">
+						<h4>Wordpress solutions</h4>
+						<h5>Integrated WP designs created with Headway</h5>
+						<img src="images/grey_line.png" class="line" alt="grey_line">
+						<p>I have integrated several uniquely designs into Wordpress templates for customers during my job in Click-A-Site as a developer. <br><br> 
+							The templates have been created using Headway template builder. <br></p><br>
+							<div class="row">
+								<div class="col-xs-6">
+
+									<a href="http://colorectalcancer.cashosting.dk" alt="Colorectalcancer" target="_blank"><button type="button" class="btn btn-block btn-info"><span class="fa fa-external-link"></span> Colorectal Cancer</button></a>
+									<a href="http://Ebberoed.cashosting.dk" alt="Ebberoed" target="_blank"><button type="button" class="btn btn-block btn-info"><span class="fa fa-external-link"></span> Ebberød</button></a>
+									<a href="http://Fjordhoj.dk" alt="Fjordhoj" target="_blank"><button type="button" class="btn btn-block btn-info"><span class="fa fa-external-link"></span> Fjordhøj</button></a>
+								</div>
+								<div class="col-xs-6">
+									<a href="http://Motivation.dk" alt="Motivation" target="_blank"><button type="button" class="btn btn-block btn-info"><span class="fa fa-external-link"></span> Motivation</button></a>
+									<a href="http://RenGaarde.dk" alt="RenGaarde.dk" target="_blank"><button type="button" class="btn btn-block btn-info"><span class="fa fa-external-link"></span> Gaarde Rengøring</button></a>
+									
+								</div>
+							</div>
+							
+							
+						</div>
+					</div><!-- col left description end -->
+					<div class="col-md-8 col-xs-12">
+						<div class="project_content_right">
+							<div class="project-image">
+
+								<!-- Carousel 5-->
+								<div id="carousel5" class="carousel slide" data-ride="carousel">
+									<!-- Indicators -->
+									<ol class="carousel-indicators">
+										<li data-target="#carousel5" data-slide-to="0" class="active"></li>
+										<li data-target="#carousel5" data-slide-to="1"></li>
+										<li data-target="#carousel5" data-slide-to="2"></li>
+										<li data-target="#carousel5" data-slide-to="3"></li>
+										<li data-target="#carousel5" data-slide-to="4"></li>
+
+									</ol>
+
+									<!-- Wrapper for slides -->
+									<div class="carousel-inner" role="listbox">
+										<div class="item active">
+											<img src="images/colorectalcancer.jpg" alt="colorectalcancer">
+										</div>
+
+										<div class="item">
+											<img src="images/ebberoed.jpg" alt="ebberoed">
+										</div>
+
+										<div class="item">
+											<img src="images/fjordhoj.jpg" alt="fjordhoj">
+										</div>
+										<div class="item">
+											<img src="images/motivation.jpg" alt="motivation">
+										</div>
+										<div class="item">
+											<img src="images/rengaarde.jpg" alt="rengaarde">
+										</div>
+
+									</div>
+
+									<!-- Left and right controls -->
+								<!-- <a class="left carousel-control" href="#carousel3" role="button" data-slide="prev">
+									<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+									<span class="sr-only">Previous</span>
+								</a>
+								<a class="right carousel-control" href="#carousel3" role="button" data-slide="next">
+									<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+									<span class="sr-only">Next</span>
+								</a> -->
+							</div><!-- Carousel 5 End -->
+
+						</div>
+					</div>
+				</div><!-- col right image end -->
+				
+			</div><!-- row end -->
+		</div><!-- container end -->
+	</article><!-- article end -->
+	<!-- Project 5 end -->
 
 </div> <!-- #page Projects END -->  
